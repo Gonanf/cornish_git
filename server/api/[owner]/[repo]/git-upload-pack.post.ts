8604444932ec2e79514bc1b8f732770a6a44ec83
Gonanf <gabysolotorevsky100@gmail.com>
2025-10-27 08:32:04 -0300

    testing(ls-refs)

--- server/api/[owner]/[repo]/git-upload-pack.post.ts
+++ server/api/[owner]/[repo]/git-upload-pack.post.ts
@@ -6,6 +6,9 @@ export default eventHandler(async (event) => {
         statusMessage: 'Not supported protocol version',
         })
     }
+
+    const owner = getRouterParam(event,'owner');
+    const repo = getRouterParam(event,'repo');
     const raw_body = await readRawBody(event);
 
     const data = new GitPKT().parsePKT(new TextEncoder().encode(raw_body)).parseCommand();
@@ -21,6 +24,24 @@ export default eventHandler(async (event) => {
     
     if (data!.command === "ls-refs"){
         const CORNISH_GIT = event.context.cloudflare.env
-        console.log(CORNISH_GIT);
+        const temp_id = CORNISH_GIT.DO.idFromName(owner + "/" + repo);
+        const stub = CORNISH_GIT.DO.get(temp_id);
+        const head = await stub.storage.get("head");
+        const refs = await stub.storage.get("refs")
+        
+        const target = stored?.target || "refs/heads/main";
+        const match = refs.find((r) => r.name === target);
+        const resolved = match
+        ? ({ target, oid: match.oid } as Head)
+        : ({ target, unborn: true } as Head);
+
+        
+    return new Response(resolved, {
+      status: 200,
+      headers: {
+        "Content-Type": "application/x-git-upload-pack-result",
+        "Cache-Control": "no-cache",
+      },
+    });
     }
 })
